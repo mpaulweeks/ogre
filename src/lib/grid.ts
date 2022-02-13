a5d175e560b1061b33ce0329b61239f418bdf645
mpaulweeks <mpaulweeks@gmail.com>
2022-02-12 20:30:07 -0500

    grid logic

--- src/lib/grid.ts
+++ src/lib/grid.ts
@@ -1,15 +1,64 @@
-import { GridKey, GridPoint, HasKey } from "./types";
+import { Unit } from ".";
+import { GridKey, GridPoint } from "./types";
 
+const UpDownLeftRight: GridPoint[] = [
+  { x: 0, y: -1 },
+  { x: 0, y: 1 },
+  { x: -1, y: 0 },
+  { x: 1, y: 0 },
+];
+const Diagonals: GridPoint[] = [
+  { x: -1, y: -1 },
+  { x: -1, y: 1 },
+  { x: -1, y: 1 },
+  { x: 1, y: 1 },
+];
+const AllAdjacent = [
+  ...UpDownLeftRight,
+  ...Diagonals,
+];
+const HowizterAttack = [
+  { x: 0, y: 2 },
+  { x: 0, y: 1 },
+  { x: -1, y: 1 },
+  { x: 1, y: 1 },
+];
 
-export class Grid<T extends HasKey> {
-  private readonly data: Record<GridKey, T> = {};
+const SupplyRange: Record<Unit, GridPoint[]> = {
+  [Unit.Infantry]: UpDownLeftRight,
+  [Unit.MissleTank]: UpDownLeftRight,
+  [Unit.Gev]: Diagonals,
+  [Unit.HeavyTank]: UpDownLeftRight,
+  [Unit.Howitzer]: UpDownLeftRight,
+  [Unit.LightGev]: UpDownLeftRight,
+  [Unit.CruiseMissiles]: [],
+  [Unit.Ogre]: AllAdjacent,
+  [Unit.OgreDamaged]: UpDownLeftRight,
+};
+const AttackRange: Record<Unit, GridPoint[]> = {
+  [Unit.Infantry]: UpDownLeftRight,
+  [Unit.MissleTank]: Diagonals,
+  [Unit.Gev]: UpDownLeftRight,
+  [Unit.HeavyTank]: UpDownLeftRight,
+  [Unit.Howitzer]: HowizterAttack,
+  [Unit.LightGev]: UpDownLeftRight,
+  [Unit.CruiseMissiles]: [], // todo
+  [Unit.Ogre]: AllAdjacent, // todo
+  [Unit.OgreDamaged]: AllAdjacent,
+};
+const SpottingRange: Record<Unit, GridPoint[]> = {
+  [Unit.Infantry]: UpDownLeftRight,
+  [Unit.MissleTank]: AllAdjacent,
+  [Unit.Gev]: UpDownLeftRight,
+  [Unit.HeavyTank]: UpDownLeftRight,
+  [Unit.Howitzer]: UpDownLeftRight,
+  [Unit.LightGev]: UpDownLeftRight,
+  [Unit.CruiseMissiles]: [], // todo
+  [Unit.Ogre]: AllAdjacent, // todo
+  [Unit.OgreDamaged]: AllAdjacent,
+};
 
-  set(value: T): void {
-    this.data[value.key] = value;
-  }
-  get(key: GridKey): T | undefined {
-    return this.data[key];
-  }
+export class Grid {
 
   static parseKey(key: GridKey): GridPoint {
     const [x, y] = key.split(',');
